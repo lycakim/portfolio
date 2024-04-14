72120d7faccf61173b6e8f6838312d39f376463c
Lyca Ubay <lycaubay28@gmail.com>
2024-04-14 20:42:17 +0800

    Updated component

--- src/components/Highlights.vue
+++ src/components/Highlights.vue
@@ -1,8 +1,46 @@
 <template>
-  <div class="min-h-36 bg-[#272727] py-5 flex justify-between gap-4 w-full">
-    <img src="../assets/kopikap_thumbnail.png" class="" />
-    <img src="../assets/para_thumbnail.png" class="" />
-    <img src="../assets/tighubad_thumbnail.png" class="" />
-    <img src="../assets/geezmo_thumbnail.png" class="" />
+  <div
+    class="min-h-40 bg-[#272727] py-10 flex justify-between gap-4 w-full overflow-y-hidden"
+  >
+    <div class="flex justify-between gap-5 items-center mx-auto">
+      <div v-for="(image, index) in images" :key="index">
+        <img
+          :src="image.url"
+          alt="Carousel Image"
+          :class="image.class"
+          class="thumbnail transform object-cover"
+        />
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      currentIndex: 0,
+      images: [
+        {
+          url: require("@/assets/kopikap_full.png"),
+          class: "rotate-3 mr-10"
+        },
+        { url: require("@/assets/geezmo_full.png"), class: "-rotate-3 mr-10" },
+        { url: require("@/assets/para_full.png"), class: "rotate-3 mr-10" },
+        {
+          url: require("@/assets/tighubad_full.png"),
+          class: "-rotate-3 mr-10"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style scoped>
+.scrollable-div {
+  max-width: 100%;
+}
+.thumbnail {
+  width: 368px;
+  height: 221px;
+}
+</style>
